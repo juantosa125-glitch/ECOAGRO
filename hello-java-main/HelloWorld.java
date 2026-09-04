b2f70728e8c6d38aa4cd32d734507e54841ab062
Tich125 <juantosa125@gmail.com>
2026-09-04 10:32:37 -0500

    frontend with bootstrap login module

---
+++ hello-java-main/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+este programa solo manda texto a la terminal :0
+*/
+public class HelloWorld {
+    static void main(String[] args){
+        int edad = 18;
+        if (edad > 18){
+            System.out.println("eres mayor de edad");
+        }else if (edad == 18){
+            System.out.println("tienes 18");
+        }else{
+            System.out.println("eres menor de edad");
+        }
+    }
+}
